2858ef9a8169ba8a6e70aa10c4284afc039cd69a
Sven Guthe <sven.guthe@intenta.de>
2022-02-07 19:57:08 +0100

    Added form Project

---
+++ formProject/src/components/SimpleInput.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+const SimpleInput = (props) => {
+
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    setEnteredNameTouched(true);
+
+    if(!enteredNameIsValid) {
+      return;
+    }
+
+    setEnteredName('');
+    setEnteredNameTouched(false);
+  };
+
+  const nameInputClasses = !nameInputIsInvalid ? 'form-control' : 'form-control invalid';
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor='name'>Your Name</label>
+        <input type='text' id='name' onChange={nameInputChangeHandler} onBlur={nameInputBlurHandler} value={enteredName}  />
+        {nameInputIsInvalid && <p className="error-text">Name must not be empty</p>}
+      </div>
+      <div className="form-actions">
+        <button disabled={!formIsValid} >Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
